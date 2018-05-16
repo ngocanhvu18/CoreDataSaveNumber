7fbca26c4d76e88679fcc5cc3efea43cfef101cf
Đai ca Anh <ngocanhvu18ict@gmail.com>
2018-05-16 09:41:23 +0700

    lol

---
+++ CoreDataSaveNumber/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CoreDataSaveNumber
+//
+//  Created by NgocAnh on 5/16/18.
+//  Copyright © 2018 NgocAnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imputView: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    func display() {
+        do {
+            guard let entity = try AppDelegate.context.fetch(Entity.fetchRequest()) as? [Entity] else {return}
+                imputView.text = String(entity.last?.number ?? 0)
+        }
+        catch{
+            print("No Data")
+        }
+    }
+
+    @IBAction func showButton(_ sender: UIButton) {
+        let entity = Entity(context: AppDelegate.context)
+        entity.number = Int32(Int(imputView.text ?? "") ?? 0 )
+        AppDelegate.saveContext()
+    }
+    
+}
+
